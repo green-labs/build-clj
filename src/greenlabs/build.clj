6cc717fba2ec66955268f6296705ede8ed8759fb
Hyunwoo Nam <namenu@gmail.com>
2021-11-20 22:59:12 +0900

    serverless 설정 실행 인자 전달 가능하도록

--- src/greenlabs/build.clj
+++ src/greenlabs/build.clj
@@ -6,9 +6,9 @@
   "
   (:require [clojure.tools.build.api :as b]
             [clojure.data.json :as json]
+            [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]
-            [clojure.string :as str]
-            [clojure.java.io :as io]))
+            [clojure.string :as str]))
 
 (def commit-hash (-> (sh "git" "rev-parse" "--short" "HEAD")
                      :out
@@ -17,25 +17,48 @@
 (def basis (b/create-basis {:project "deps.edn"}))
 (def uber-file (format "target/%s.jar" commit-hash))
 
+;; 람다 실행을 위한 최소한의 권한이 부여된 IAM Role 입니다.
+(def aws-role "arn:aws:iam::887960154422:role/lambda-exec-role")
+
 (defn clean [_]
   (b/delete {:path "target"}))
 
 (defn uber [_]
   (clean nil)
-  (b/write-file {:path "target/target.json"
+  (b/write-file {:path   "target/target.json"
                  :string (json/write-str {:file uber-file :version commit-hash}
                                          :escape-slash false)})
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs   ["src" "resources"]
                :target-dir class-dir})
-  (b/compile-clj {:basis basis
-                  :src-dirs ["src"]
+  (b/compile-clj {:basis     basis
+                  :src-dirs  ["src"]
                   :class-dir class-dir})
   (b/uber {:class-dir class-dir
            :uber-file uber-file
-           :basis basis}))
+           :basis     basis}))
 
 (defn serverless
-  "serverless에 필요한 설정 템플릿을 생성합니다."
-  [_]
-  (b/write-file {:path   "serverless.yml"
-                 :string (slurp (io/resource "serverless.yml"))}))
+  "serverless 기본 설정을 생성합니다.
+
+  config 에는 템플릿 변수에 해당하는 key가 있어야 합니다.
+  "
+  [{:keys [serverless]}]
+  (when (.exists (b/resolve-path "serverless.yml"))
+    (println "serverless.yml already exists.")
+    (System/exit 1))
+
+  (let [;; temporary placeholder
+        default-config {:service  "my-service"
+                        :function "my-fn"
+                        :file     "my-file"
+                        :role     aws-role}
+        config         (merge default-config serverless)
+
+        yaml           (slurp (io/resource "serverless.yml"))
+        output         (reduce-kv
+                         (fn [s k v]
+                           (str/replace s (str "{{" (name k) "}}") v))
+                         yaml
+                         config)]
+    (b/write-file {:path   "serverless.yml"
+                   :string output})))
